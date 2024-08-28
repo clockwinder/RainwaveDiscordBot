98efdfbfd7f75467457aff7a3271e5b09deacbc9
Roach <coldshouldermedia@yahoo.com>
2024-08-28 16:03:27 -0600

    Sync thread restart added

--- rainwavebot.py
+++ rainwavebot.py
@@ -48,11 +48,14 @@ def getChannelList():
 
 def checkSyncThreadIsAlive():
     try:
-        current.selectedStream._sync_thread.is_alive()
+        if current.selectedStream._sync_thread.is_alive() == False:
+            current.selectedStream.start_sync()
+            print("RW Sync Restarted")
     except Exception as returnedException:
         #print(f"checkSyncThreadIsAlive error: {returnedException}") #NOTE Not required here, but I want to keep it noted as an example.
         #traceback.print_exc() #NOTE Not required here, but I want to keep it noted as an example.
         current.selectedStream.start_sync()
+        print("RW Sync Started")
 
 async def postCurrentlyListening(ctx = None, stopping=False):
     checkSyncThreadIsAlive()
