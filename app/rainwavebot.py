4de33f7409e242008bcb7be4d64daeaf691dc94f
Clockwinder <coldshouldermedia@yahoo.com>
2026-03-05 16:49:04 -0700

    Bump version number to 1.0.3

--- app/rainwavebot.py
+++ app/rainwavebot.py
@@ -19,7 +19,7 @@ from rainwaveclient import RainwaveClient #NOTE Command to upgrade the rainwavec
 import load_config.load_config
 
 #Global Constants
-VERSION_NUMBER = "v1.0.2"
+VERSION_NUMBER = "v1.0.3"
 MINIMUM_REFRESH_DELAY = 6
 PROJECT_HOME_PAGE = "https://github.com/clockwinder/RainwaveDiscordBot"
 
